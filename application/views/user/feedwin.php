463ccac151a1ddcaa34b7c8d7f3c58381ae37d3f
nearTeacher <bondu.seo@gmail.com>
2022-07-01 11:53:20 +0900

    대폭수정전 저장

--- application/views/user/feedwin.php
+++ application/views/user/feedwin.php
@@ -3,7 +3,7 @@
     <div class="w100p_mw614">
         <div class="d-flex flex-row">
             <div class="d-flex flex-column justify-content-center">
-                <div class="circleimg h150 w150 pointer feedwin">
+                <div class="circleimg h150 w150 pointer feedwin" data-bs-toggle="modal" data-bs-target="#changeProfile">
                     <img src='<?= _DIR ?>/static/img/profile/<?= $this->data->iuser ?>/<?= $this->data->mainimg ?>' onerror='this.error=null;this.src="<?= _DIR ?>/static/img/profile/defaultProfileImg_100.png"'>
                 </div>
             </div>
@@ -12,3 +12,20 @@
         </div>
     </div>
 </div>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="changeProfile" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-400">
+        <div class="modal-content">
+
+            <div class="modal-body p-0">
+                <div class="modal-title bold">프로필 사진 바꾸기</div>
+                <div class="profileUpload bold pointer">사진 업로드</div>
+                <div class="profileDelete bold pointer">현재 사진 삭제</div>
+                <div class="pointer" data-bs-dismiss="modal" aria-label="Close">취소</div>
+
+            </div>
+        </div>
+    </div>
